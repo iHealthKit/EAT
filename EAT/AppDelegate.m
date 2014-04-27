d8bbd8e0d0e4e5a229000042cc42c695db049865
lukaskollmer <lukas.kollmer@me.com>
2014-04-27 12:34:51 +0200

    added CocoaLumberjack

--- EAT/AppDelegate.m
+++ EAT/AppDelegate.m
@@ -7,8 +7,16 @@
 //
 
 #import "AppDelegate.h"
+#import "DDASLLogger.h"
+#import "DDTTYLogger.h"
 
 @implementation AppDelegate
+
++ (void)initialize
+{
+    [DDLog addLogger:[DDASLLogger sharedInstance]];
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
